f78010024117bc5069cfd6ca1331bb14b7917238
Lancey6 <david.thomas.reeve@gmail.com>
2015-11-16 17:39:11 -0500

    Added simple routing, views, and style sheet

---
+++ app.js
@@ -0,0 +1,31 @@
+var bodyparser = require('body-parser');
+var chalk = require('chalk');
+var express = require('express');
+var swig = require('swig');
+
+var app = express();
+
+// app config
+app.engine( 'html', swig.renderFile );
+app.set( 'view engine', 'html' );
+app.set( 'views', process.cwd() + '/views' );
+
+swig.setDefaults( { cache: false } );
+
+// log request
+app.use('/', function( req, res, next ) {
+  res.on('finish', function() {
+    console.log( chalk.green( req.method ), req.path, chalk.yellow( res.statusCode ) );
+  } );
+  next();
+} );
+
+// body parsing middleware
+app.use(bodyparser.urlencoded( { extended: false } ));
+app.use(bodyparser.json());
+app.use('/static', express.static( __dirname + '/public') );
+
+// start server
+var server = app.listen( 3000, function() {
+  console.log( "Listening on port 3000" );
+} );
